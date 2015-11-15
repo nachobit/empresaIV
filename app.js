f784d6685db8cfcecc28b38165c6b5ea1f230554
nachobit <Nacho@MacBook-Pro-de-Ignacio.local>
2015-11-15 21:41:25 +0100

    update app & proc

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
-var express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
-    methodOverride  = require("method-override"),
-    mongoose        = require('mongoose');
+var DocumentDBClient = require('documentdb').DocumentClient;
+var config = require('./config');
+var TaskList = require('./routes/tasklist');
+var TaskDao = require('./models/taskDao');
 
-// Connection to DB
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
-  if(err) throw err;
-  console.log('Connected to Database');
-});
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-// Middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+//CONEXION CON BASE DATOS MongoDB
+var mongoose = require('mongoose');  
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Import Models and controllers
-var models     = require('./models/tvshow')(app, mongoose);
-var TVShowCtrl = require('./controllers/tvshows');
-
-// Example Route
 var router = express.Router();
-router.get('/', function(req, res) {
-  res.send("Hello world!");
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+var docDbClient = new DocumentDBClient(config.host, {
+    masterKey: config.authKey
 });
+var taskDao = new TaskDao(docDbClient, config.databaseId, config.collectionId);
+var taskList = new TaskList(taskDao);
+taskDao.init();
+
+app.get('/', taskList.showTasks.bind(taskList));
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 app.use(router);
 
-// API routes
-var tvshows = express.Router();
-
-tvshows.route('/tvshows')
-  .get(TVShowCtrl.findAllTVShows)
-  .post(TVShowCtrl.addTVShow);
-
-tvshows.route('/tvshows/:id')
-  .get(TVShowCtrl.findById)
-  .put(TVShowCtrl.updateTVShow)
-  .delete(TVShowCtrl.deleteTVShow);
-
-app.use('/api', tvshows);
-//app.use(tvshows);
-
-// Start server
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
 });
+
+module.exports = app;
